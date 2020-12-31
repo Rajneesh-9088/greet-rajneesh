ccdc6a7c205ae90b1d5bcedc90a1c7b87e9465fb
proraj9088 <rp8735685@gmail.com>
2021-01-01 01:53:39 +0530

    using port heroku
    Signed-off-by: proraj9088 <rp8735685@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 4444;
+
 app.get('/',(req,res) => {
     res.send('Hello  this is Rajneesh');
 })
 
 
-app.listen(4444,(req,res) =>{
-    console.log('started on http://localhost:4444');
+app.listen(PORT,(req,res) =>{
+    console.log(`started on http://localhost:${PORT}`);
 })
